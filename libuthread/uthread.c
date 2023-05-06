4b4d253ab31b9ba9299ef6ce818212b611758617
Shyam Agarwal <shyagarwal@ucdavis.edu>
2023-05-06 08:14:31 -0700

    Phase 2 hopefully finished, need to add more test cases

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -40,9 +40,10 @@ void uthread_yield(void) {
     struct uthread_tcb* current_uthread = uthread_current();
     struct uthread_tcb* next_uthread = NULL;
     queue_enqueue(scheduling_queue, current_uthread);
-    queue_dequeue(scheduling_queue, &next_uthread);
+    queue_dequeue(scheduling_queue, (void*)&next_uthread);
     currently_executing_thread = next_uthread;
-    uthread_ctx_switch(current_uthread, next_uthread);
+    uthread_ctx_switch(current_uthread->thread_context,
+                       next_uthread->thread_context);
 }
 
 void uthread_exit(void) {
@@ -50,22 +51,26 @@ void uthread_exit(void) {
     struct uthread_tcb* current_uthread = uthread_current();
     struct uthread_tcb* next_uthread = NULL;
     queue_enqueue(zombie_queue, current_uthread);
-    queue_dequeue(scheduling_queue, &next_uthread);
+    queue_dequeue(scheduling_queue, (void*)&next_uthread);
     currently_executing_thread = next_uthread;
-    uthread_ctx_switch(current_uthread, next_uthread);
+    uthread_ctx_switch(current_uthread->thread_context,
+                       next_uthread->thread_context);
 }
 
 int uthread_create(uthread_func_t func, void* arg) {
     /* TODO Phase 2 */
     struct uthread_tcb* created_thread =
         (struct uthread_tcb*)malloc(sizeof(struct uthread_tcb));
-
     /*Memory allocation error*/
-    if (created_thread = NULL) return -1;
+    if (created_thread == NULL) {
+        return -1;
+    }
 
     queue_enqueue(scheduling_queue, (void*)(created_thread));
     created_thread->thread_state = Ready;
     created_thread->thread_stack_top = uthread_ctx_alloc_stack();
+    created_thread->thread_context =
+        (uthread_ctx_t*)malloc(sizeof(uthread_ctx_t));
     int context_creation_success =
         uthread_ctx_init(created_thread->thread_context,
                          created_thread->thread_stack_top, func, arg);
@@ -80,16 +85,27 @@ int uthread_run(bool preempt, uthread_func_t func, void* arg) {
     /* TODO Phase 2 */
     scheduling_queue = queue_create();
     zombie_queue = queue_create();
-    /* There is no need to initialize currently executing thread because as soon
-     * as we perform uthread_run(), we also perform uthread_yield() as
+    /* There is no need to initialize currently executing thread because as
+     * soon as we perform uthread_run(), we also perform uthread_yield() as
      * queue_length is more than or equal to 1 everytime*/
+    currently_executing_thread =
+        (struct uthread_tcb*)malloc(sizeof(struct uthread_tcb));
+    currently_executing_thread->thread_context =
+        (uthread_ctx_t*)malloc(sizeof(uthread_ctx_t));
     int initial_thread_success = uthread_create(func, arg);
     if (initial_thread_success == -1) {
         return -1;
     }
+
     while (queue_length(scheduling_queue)) {
         uthread_yield();
     }
+
+    /*Free the memory for currently active thread*/
+    free(currently_executing_thread->thread_context);
+    free(currently_executing_thread);
+
+    /*Free memory for the queue objects*/
     queue_destroy(scheduling_queue);
     queue_destroy(zombie_queue);
     return 0;
