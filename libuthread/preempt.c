53fa22e129af41520ada1c9a12cb9bd6895189dc
Hum4n01d <17228477+Hum4n01d@users.noreply.github.com>
2023-05-08 10:10:46 +0000

    Implement premption timer

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -23,24 +23,89 @@ preemption should be enabled. What does this mean?
 #define HZ 100
 
 struct sigaction sa;
+bool global_preempt = false;
 
 void interrupt_handler(int signum) {
-    /* User-defined function*/
+    // Yield to next thread
     uthread_yield();
 }
 
-void preempt_disable(void) { /* TODO Phase 4 */
-}
-
-void preempt_enable(void) { /* TODO Phase 4 */
-}
-
 void preempt_start(bool preempt) {
-    /* TODO Phase 4 */
+    // Update global preempt flag
+    global_preempt = preempt;
+
+    if (!global_preempt) return;
+
+    // Set up signal handler
     sa = {0};
     sa.sa_handler = interrupt_handler;
     sigaction(SIGVTALRM, &sa, NULL);
+
+    // Configure timer to expire every 10 ms (100 times per second)
+    struct itimerval timer;
+    timer.it_value.tv_sec = 0;
+    timer.it_value.tv_usec = 10000;
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = 10000;
+
+    setitimer(ITIMER_VIRTUAL, &timer, NULL);
 }
 
-void preempt_stop(void) { /* TODO Phase 4 */
+/*
+ * preempt_stop - Stop thread preemption
+ *
+ * Restore previous timer configuration, and previous action associated to
+ * virtual alarm signals.
+ */
+void preempt_stop(void) {
+    if (!global_preempt) return;
+
+    // Restore previous timer configuration
+    struct itimerval timer;
+    timer.it_value.tv_sec = 0;
+    timer.it_value.tv_usec = 0;
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = 0;
+
+    // Disable timer
+    setitimer(ITIMER_VIRTUAL, &timer, NULL);
+
+    // TODO: Restore previous action associated to virtual alarm signals
+}
+
+/*
+The two other functions that you must implement are meant to enable or disable
+preemption. For that, you will need to be able to block or unblock signals of
+type SIGVTALRM.
+*/
+
+void preempt_enable(void) {
+    if (!global_preempt) return;
+
+    sigset_t sigset;
+
+    // Initialize signal set
+    sigemptyset(&sigset);
+
+    // Add SIGVTALRM to signal set
+    sigaddset(&sigset, SIGVTALRM);
+
+    // Unblock SIGVTALRM
+    sigprocmask(SIG_UNBLOCK, &sigset, NULL);
+    /
+}
+
+void preempt_disable(void) {
+    if (!global_preempt) return;
+
+    sigset_t sigset;
+
+    // Initialize signal set
+    sigemptyset(&sigset);
+
+    // Add SIGVTALRM to signal set
+    sigaddset(&sigset, SIGVTALRM);
+
+    // Block SIGVTALRM
+    sigprocmask(SIG_BLOCK, &sigset, NULL);
 }
