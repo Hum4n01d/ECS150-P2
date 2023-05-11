ac5b92ab9764785e7b8538484e15050d34fda6bd
Hum4n01d <17228477+Hum4n01d@users.noreply.github.com>
2023-05-11 14:56:57 -0700

    Add alarm and timer restoration test (and fix)

--- apps/preempt_test.c
+++ apps/preempt_test.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <uthread.h>
+#include <signal.h>
+#include <sys/time.h>
 
 void busy_loop(void) {
     int i = 0;
@@ -34,8 +36,45 @@ void thread_func2(void *arg) {
     printf("=== Thread 2 done ===\n");
 }
 
+void dummy_handler(int signum) {
+    (void)signum;
+}
+
 int main(void) {
+    // Create dummy SIGVTALRM handler
+    struct sigaction sa;
+    sa.sa_handler = dummy_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGVTALRM, &sa, NULL);
+
+    // Create dummy timer
+    struct itimerval timer;
+    timer.it_value.tv_sec = 0;
+    timer.it_value.tv_usec = 0;
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = 0;
+    setitimer(ITIMER_VIRTUAL, &timer, NULL);
+
+    // Run thread_func2
     uthread_run(true, thread_func2, NULL);
+
+    // Verify that placeholder SIGVTALRM handler was restored
+    struct sigaction old_sa;
+    sigaction(SIGVTALRM, NULL, &old_sa);
+    if (old_sa.sa_handler != dummy_handler) {
+        printf("ERROR: SIGVTALRM handler was not restored\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Verify that placeholder timer was restored
+    struct itimerval old_timer;
+    setitimer(ITIMER_VIRTUAL, NULL, &old_timer);
+    if (old_timer.it_value.tv_sec != 0 || old_timer.it_value.tv_usec != 0 ||
+        old_timer.it_interval.tv_sec != 0 || old_timer.it_interval.tv_usec != 0) {
+        printf("ERROR: timer was not restored\n");
+        exit(EXIT_FAILURE);
+    }
 
     printf("Test complete.\n");
 
