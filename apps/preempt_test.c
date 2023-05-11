2ccce975887285def4b84615c28b0b1cd6843144
Hum4n01d <17228477+Hum4n01d@users.noreply.github.com>
2023-05-11 12:34:16 -0700

    Fix preempt_test int issue, make preemption more clear

--- apps/preempt_test.c
+++ apps/preempt_test.c
@@ -3,24 +3,19 @@
 #include <uthread.h>
 
 void busy_loop(void) {
-    intmax_t i = 0;
-    while (i < 10000000000) {
+    int i = 0;
+    while (i < 1000000000) {
         i++;
+
+        if (i % 100000000 == 0) {
+            printf("Busy loop %d\n", i / 100000000);
+        }
     }
-}
-
-void thread_func2(void *arg) {
-    (void)arg;
-
-    printf("Thread 2 starting...\n");
-    busy_loop();
-    printf("Thread 2 done.\n");
 }
 
 void thread_func1(void *arg) {
     (void)arg;
 
-    uthread_create(thread_func2, NULL);
 
     printf("Thread 1 starting...\n");
     for (int i = 0; i < 5; i++) {
@@ -30,8 +25,18 @@ void thread_func1(void *arg) {
     printf("Thread 1 done.\n");
 }
 
+void thread_func2(void *arg) {
+    (void)arg;
+
+    uthread_create(thread_func1, NULL);
+
+    printf("Thread 2 starting...\n");
+    busy_loop();
+    printf("Thread 2 done.\n");
+}
+
 int main(void) {
-    uthread_run(true, thread_func1, NULL);
+    uthread_run(true, thread_func2, NULL);
 
     printf("Test complete.\n");
 
